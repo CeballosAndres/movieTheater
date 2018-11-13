6d64b227526284a47a388def1a92801fd96c829b
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2018-11-12 18:26:20 -0600

    add, and view movies

--- src/movietheatersystem/ListaPeliculas.java
+++ src/movietheatersystem/ListaPeliculas.java
@@ -6,12 +6,7 @@ public class ListaPeliculas {
 
     Pelicula inicio, fin;
     Metodos m = new Metodos();
-
-    public ListaPeliculas(Pelicula inicio, Pelicula fin) {
-        this.inicio = inicio;
-        this.fin = fin;
-    }
-
+    
     public Pelicula getInicio() {
         return inicio;
     }
@@ -51,9 +46,52 @@ public class ListaPeliculas {
     }
 
     public void agregarPelicula() {
-        String nombre = m.entradaTexto();
-        if (this.buscarPorNombre(nombre) != null) {
-            
+        String nombre = "", genero = "", director = "";
+        int duracion = 0;
+        System.out.print("Nombre");
+        nombre = m.entradaTexto();
+        if (this.buscarPorNombre(nombre) == null) {
+            System.out.print("Genero");
+            genero = m.entradaTexto();
+            System.out.print("Director");
+            director = m.entradaTexto();
+            System.out.print("duracion");
+            duracion = m.entradaEntero();
+            this.agregarFinal(new Pelicula(nombre, genero, director, duracion));
+        } else {
+            System.out.println("Esa pelicula ya existe.");
+        }
+    }
+
+    public void eliminarPelicula() {
+        String nombre;
+        Pelicula eliminar;
+        System.out.print("Nombre");
+        nombre = m.entradaTexto();
+        if ((eliminar = this.buscarPorNombre(nombre)) != null) {
+            eliminar.getAtras().setSigue(eliminar.getSigue());
+            eliminar.getSigue().setAtras(eliminar.getAtras());
+        }
+        System.out.println("Eliminado");
+    }
+    public void mostrarPeliculas() {
+        if (this.vacia()) {
+            System.out.println("No hay peliculas registradas.");
+        } else {
+            Pelicula aux = this.inicio;
+            System.out.printf("%-20s", "Nombre");
+            System.out.printf("%-20s", "Director");
+            System.out.printf("%-10s", "Genero");
+            System.out.printf("%-4s", "Duración");
+            System.out.println();
+            while (aux != null) {
+                System.out.printf("%-20s", aux.getNombre());
+                System.out.printf("%-20s", aux.getDirector());
+                System.out.printf("%-10s", aux.getGenero());
+                System.out.printf("%-4s", aux.getDuracion());
+                System.out.println();
+                aux = aux.getSigue();
+            }
         }
     }
 
