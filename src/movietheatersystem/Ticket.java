96bcb2bd3f4af36947cbc4b795d4ebb6944a0bce
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2018-11-15 15:55:52 -0600

    Adding ticket list

--- src/movietheatersystem/Ticket.java
+++ src/movietheatersystem/Ticket.java
@@ -1,22 +1,101 @@
 package movietheatersystem;
 public class Ticket {
-    Ticket next;
-    String folio;
-    String nameOwner;
-    int ageOwner;
-    int ticketStandard;
-    int ticketBoy;
-    int ticketElderly;
-    float total;
+    private Ticket next, back;
+    private String folio;
+    private String name;
+    private int age;
+    private int ticketStandard;
+    private int ticketBoy;
+    private int ticketElderly;
+    private float total;
     
-    public Ticket(){
+    public Ticket(String Name){
         this.next = null;
+        this.back = null;
         this.folio = null;
-        this.nameOwner = null;
-        this.ageOwner = 0;
+        this.name = name;
+        this.age = 0;
         this.ticketStandard = 0;
         this.ticketBoy = 0;
         this.ticketElderly = 0;
         this.total = 0;
     }
+
+    public Ticket getBack() {
+        return back;
+    }
+
+    public void setBack(Ticket back) {
+        this.back = back;
+    }
+    
+    
+
+    public Ticket getNext() {
+        return next;
+    }
+
+    public String getFolio() {
+        return folio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getTicketStandard() {
+        return ticketStandard;
+    }
+
+    public int getTicketBoy() {
+        return ticketBoy;
+    }
+
+    public int getTicketElderly() {
+        return ticketElderly;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setNext(Ticket next) {
+        this.next = next;
+    }
+
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTicketStandard(int ticketStandard) {
+        this.ticketStandard = ticketStandard;
+    }
+
+    public void setTicketBoy(int ticketBoy) {
+        this.ticketBoy = ticketBoy;
+    }
+
+    public void setTicketElderly(int ticketElderly) {
+        this.ticketElderly = ticketElderly;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+    
+    
+    
+    
 }
--- src/movietheatersystem/TicketList.java
+++ src/movietheatersystem/TicketList.java
@@ -1,9 +1,54 @@
 package movietheatersystem;
 
-/**
- *
- * @author ac
- */
 public class TicketList {
+
+    Util util = new Util();
+    private Ticket first, end;
+
+    public TicketList() {
+        this.first = null;
+        this.end = null;
+    }
+
+    public Util getUtil() {
+        return util;
+    }
+
+    public Ticket getFirst() {
+        return first;
+    }
+
+    public Ticket getEnd() {
+        return end;
+    }
+
+    public void setUtil(Util util) {
+        this.util = util;
+    }
+
+    public void setFirst(Ticket first) {
+        this.first = first;
+    }
+
+    public void setEnd(Ticket end) {
+        this.end = end;
+    }
     
+    
+
+    public boolean empty() {
+        return this.first == null;
+    }
+
+    private void addEnd(Ticket ticket) {
+        if (this.empty()) {
+            this.first = ticket;
+            this.end = ticket;
+        } else {
+            ticket.setBack(this.end);
+            this.first.setFirst(nuevo);
+            this.fin = nuevo;
+        }
+        cantidadPeliculas++;
+    }
 }
