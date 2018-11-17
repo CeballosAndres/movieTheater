b5a9fa1ac64273af35e239d3d012dbf80e0c67aa
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2018-11-17 01:19:56 -0600

    fixing

--- src/movietheatersystem/Ticket.java
+++ src/movietheatersystem/Ticket.java
@@ -41,28 +41,27 @@ public class Ticket {
 
     public void mostrarTicket(float costo, float descuentoNinos, float descuentoTercera) {
         System.out.println();
-        System.out.printf("|  %-5s  ", "Name");
+        System.out.printf("|  %-10s  ", "Nombre");
         System.out.printf("|  %-6s  ", "Folio");
-        System.out.printf("|  %-14s  ", "ticketStandard");
-        System.out.printf("|  %-10s  ", "ticketKids");
-        System.out.printf("|  %-14s  ", "ticketElderly");
-        System.out.printf("|  %-6s  ", "total");
+        System.out.printf("|  %-14s  ", "Estandar");
+        System.out.printf("|  %-10s  ", "Niños");
+        System.out.printf("|  %-14s  ", "A. mayor");
+        System.out.printf("|  %-6s  |", "Total");
         System.out.println();
 
-        System.out.printf("|  %-6s  ", " ");
-        System.out.printf("|  %-7s  ", " ");
-        System.out.printf("|  %-15s  ", this.ticketStandard);
-        System.out.printf("|  %-11s  ", this.ticketKids);
-        System.out.printf("|  %-15s  ", this.ticketElderly);
-        System.out.printf("|  %-7s  ", "  ");
-
-        System.out.println();
-        System.out.printf("| %-6s  ", this.name);
+        System.out.printf("| %-10s  ", this.name);
         System.out.printf("| %-7s  ", this.folio);
+        System.out.printf("| %-15s  ", this.ticketStandard);
+        System.out.printf("| %-11s  ", this.ticketKids);
+        System.out.printf("| %-15s  ", this.ticketElderly);
+        System.out.printf("| %-7s  |", "");
+
+        System.out.println();
+        System.out.printf("| %-21s  ", "");
         System.out.printf("| %-15s  ", this.ticketStandard * costo);
         System.out.printf("| %-11s  ", this.ticketKids * costo * ((100 - descuentoNinos) / 100));
         System.out.printf("| %-15s  ", this.ticketElderly * costo * ((100 - descuentoTercera) / 100));
-        System.out.printf("| %-7s  ", this.total);
+        System.out.printf("| %-7s  |", this.total);
         System.out.println();
     }
 
